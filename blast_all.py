811c86e03838449fddcb3492bea03554f6cc02e5
jon <jbrenluc@gmail.com>
2017-06-11 23:55:25 -0500

    i truly hate whoever invented sig ws

--- blast_all.py
+++ blast_all.py
@@ -9,19 +9,20 @@ for f in in_files:
     hits = []
     for seq in seqs:
         blast_cmd = subprocess.Popen([
-                    'blastn',
-                    '-query', 'sequence.fasta',
-                    '-db', 'Viral',
-                    '-outfmt', '10 qseqid stitle length pident bitscore',
-                    '-max_target_seqs', '1'
-		    ],
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	stdout, stderr = blast_cmd.communicate()
+            'blastn',
+            '-query', 
+            'sequence.fasta',
+            '-db', 'Viral',
+            '-outfmt', '10 qseqid stitle length pident bitscore',
+            '-max_target_seqs', '1'
+	    ],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = blast_cmd.communicate()
 	# Clean output and append to results list.
-    	hits.append(stdout)
-	with open('blast-'+'/'.strip(f)[-1]) as out:
-	    out.write('qseqid, stitle,, length, pident, bitscore\n')
-	    for hit in hits:
-		out.write(hit+'\n')
+        hits.append(stdout)
+        with open('blast-'+'/'.strip(f)[-1]) as out:
+            out.write('qseqid, stitle,, length, pident, bitscore\n')
+            for hit in hits:
+                out.write(hit+'\n')
 
 

f048abdd964b4ac7d34332d2ee48702d58333d3f
Catherine Putonti <cputonti@bio>
2017-06-12 01:03:01 -0500

    life is pain

--- blast_all.py
+++ blast_all.py
@@ -1,13 +1,17 @@
 from Bio import SeqIO
 import subprocess
 from glob import glob
+import os 
 # Input dir. Include wildcard to get all files.
 in_dir = 'urinary_bacteria_out/all_results/*'
 in_files = glob(in_dir)
 for f in in_files:
-    seqs = SeqIO.parse('sequence.fasta', 'fasta')
+    seqs = SeqIO.parse(f, 'fasta')
     hits = []
-    for seq in seqs:
+    for s in seqs:
+        with open('sequence.fasta', 'w') as out:
+	    out.write(str(s.seq))
+ 
         blast_cmd = subprocess.Popen([
             'blastn',
             '-query', 
@@ -20,9 +24,10 @@ for f in in_files:
         stdout, stderr = blast_cmd.communicate()
 	# Clean output and append to results list.
         hits.append(stdout)
-        with open('blast-'+'/'.strip(f)[-1]) as out:
-            out.write('qseqid, stitle,, length, pident, bitscore\n')
-            for hit in hits:
-                out.write(hit+'\n')
+    outfile = 'blast-'+os.path.basename(f)
+    with open(outfile, 'w') as out:
+        out.write('qseqid, stitle,, length, pident, bitscore\n')
+        for hit in hits:
+            out.write(hit.decode('UTF8').strip()+'\n')
 
 

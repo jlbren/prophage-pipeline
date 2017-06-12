de970efd1e30e9b2b5226c33fa2eab30b8294d14
jon <jbrenluc@gmail.com>
2017-06-11 23:49:59 -0500

    fixed spacing

--- blast_all.py
+++ blast_all.py
@@ -1,14 +1,14 @@
 from Bio import SeqIO
 import subprocess
-from glob import glob 
-# Input dir. Include wildcard to get all files. 
+from glob import glob
+# Input dir. Include wildcard to get all files.
 in_dir = 'urinary_bacteria_out/all_results/*'
 in_files = glob(in_dir)
 for f in in_files:
-	seqs = SeqIO.parse('sequence.fasta', 'fasta')
-	hits = []
-	for seq in seqs:
-    		blast_cmd = subprocess.Popen([
+    seqs = SeqIO.parse('sequence.fasta', 'fasta')
+    hits = []
+    for seq in seqs:
+        blast_cmd = subprocess.Popen([
                     'blastn',
                     '-query', 'sequence.fasta',
                     '-db', 'Viral',
@@ -16,13 +16,12 @@ for f in in_files:
                     '-max_target_seqs', '1'
 		    ],
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		stdout, stderr = blast_cmd.communicate()
-		# Clean output and append to results list.
-    		hits.append(stdout)
-	
-	with open('blast-'+'/'.strip(f)[-1]) as out: 
-		out.write('qseqid, stitle,, length, pident, bitscore\n')
-		for hit in hits: 
-			out.write(hit+'\n') 
-  
+	stdout, stderr = blast_cmd.communicate()
+	# Clean output and append to results list.
+    	hits.append(stdout)
+	with open('blast-'+'/'.strip(f)[-1]) as out:
+	    out.write('qseqid, stitle,, length, pident, bitscore\n')
+	    for hit in hits:
+		out.write(hit+'\n')
+
 

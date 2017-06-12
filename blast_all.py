08b7be76a9fed6bdf848aa61efbea9f531c7b854
Catherine Putonti <cputonti@bio>
2017-06-11 23:25:00 -0500

    god io wish we had a modern version opf py

--- blast_all.py
+++ blast_all.py
@@ -1,17 +1,28 @@
 from Bio import SeqIO
 import subprocess
-
-seqs = SeqIO.parse('sequence.fasta', 'fasta')
-hits = []
-for seq in seqs:
-    blast_cmd = subprocess.run([
+from glob import glob 
+# Input dir. Include wildcard to get all files. 
+in_dir = 'urinary_bacteria_out/all_results/*'
+in_files = glob(in_dir)
+for f in in_files:
+	seqs = SeqIO.parse('sequence.fasta', 'fasta')
+	hits = []
+	for seq in seqs:
+    		blast_cmd = subprocess.Popen([
                     'blastn',
                     '-query', 'sequence.fasta',
                     '-db', 'Viral',
-                    '-outfmt', '10 stitle length pident bitscore',
+                    '-outfmt', '10 qseqid stitle length pident bitscore',
                     '-max_target_seqs', '1'
-                   ],
-                   stdout=subprocess.PIPE)
+		    ],
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		stdout, stderr = blast_cmd.communicate()
+		# Clean output and append to results list.
+    		hits.append(stdout)
+	
+	with open('blast-'+'/'.strip(f)[-1]) as out: 
+		out.write('qseqid, stitle,, length, pident, bitscore\n')
+		for hit in hits: 
+			out.write(hit+'\n') 
+  
 
-    hits.append(blast_cmd.stdout.decode('utf-8').strip()) # Clean output and append
-print(str(hits[0]))
